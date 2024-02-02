12f268124a9009282a64f0e960d763520c4bd0f8
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-02 14:13:53 -0500

    add post video

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const cors = require("cors");
+const videosRoutes = require("./routes/videos.js");
+
+const PORT = process.env.PORT || 8082;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/videos", videosRoutes);
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
--- routes/videos.js
+++ routes/videos.js
@@ -1,4 +1,57 @@
+// import uploadImg from "../public/images/Upload-video-preview.jpg";
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const crypto = require("crypto");
+const path = require("path");
+
+const FILE_PATH = "./data/videos.json";
+
+const readVideos = () => {
+  const videosData = fs.readFileSync(FILE_PATH);
+  const parsedVideos = JSON.parse(videosData);
+  return parsedVideos;
+};
+
+//GET all videos
+router.get("/", (req, res) => {
+  const videos = readVideos();
+  res.status(200).json(videos);
+});
+
+//GET single video
+router.get("/:videoId", (req, res) => {
+  const videos = readVideos();
+  const singleVideo = videos.find((video) => video.id === req.params.videoId);
+  res.json(singleVideo);
+});
+
+//POST video
+router.post("/", (req, res) => {
+  //   const defaultUploadImg = path.join(
+  //     __dirname,
+  //     "../public/images/Upload-video-preview.jpg"
+  //     // "http://localhost:8080${video.image}"
+  //   );
+  const newVideo = {
+    id: crypto.randomUUID(),
+    title: req.body.title,
+    channel: "BrainStation",
+    image: req.body.image,
+    description: req.body.description,
+    views: "0",
+    likes: "0",
+    duration: "3.29",
+    video: "https://project-2-api.herokuapp.com/stream", //
+    timestamp: new Date(),
+    comments: [],
+  };
+
+  const videos = readVideos();
+  videos.push(newVideo);
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+
+  res.status(201).json(newVideo);
+});
+
+module.exports = router;
